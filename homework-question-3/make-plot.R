9e49e79be8abb6c679fcd4770a933e8a576f05cc
georgerobson1 <101802416+georgerobson1@users.noreply.github.com>
2022-03-17 15:44:56 +0000

    Update make-plot.R
    png is not compatible with my computer, as such Ichanged the format of the graph to pdf

--- homework-question-3/make-plot.R
+++ homework-question-3/make-plot.R
@@ -22,7 +22,7 @@ g <- ggplot(
   theme_classic() +
   theme(axis.text.x = element_text(angle = -45))
 
-ggsave(filename = "result.png",
+ggsave(filename = "result.pdf",
        plot = g,
        height = 10.5, width = 14.8,
        units = "cm")
